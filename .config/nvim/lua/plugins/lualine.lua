0b207cdd95845b7aa9885861aefefd09509ea759
iiixio <152221911+iiixio@users.noreply.github.com>
2024-05-30 14:13:42 -0400

    update to trouble v3 in default-->remove barbecue.nvim

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,25 +1,16 @@
-local icons = require("lazyvim.config").icons
-
 return {
-  {
-    "nvim-lualine/lualine.nvim",
+	{
+		"nvim-lualine/lualine.nvim",
 
-    opts = function(_, opts)
-      opts.sections.lualine_c = {
-        {
-          "diagnostics",
-          symbols = {
-            error = icons.diagnostics.Error,
-            warn = icons.diagnostics.Warn,
-            info = icons.diagnostics.Info,
-            hint = icons.diagnostics.Hint,
-          },
-        },
-        {},
-      }
-      opts.sections.lualine_z = {
-        { "filetype", icon_only = true, colored = false, separator = "", padding = { left = 1, right = 0 } },
-      }
-    end,
-  },
+		opts = function(_, opts)
+			opts.sections.lualine_z = {
+				{
+					"buffers",
+					icons_enabled = false,
+					mode = 1,
+					max_length = vim.o.columns * 1 / 8,
+				},
+			}
+		end,
+	},
 }
