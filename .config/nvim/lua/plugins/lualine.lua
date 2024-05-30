b0bd8233e0a68e23f62d5634b4846978760e03dd
iiixio <152221911+iiixio@users.noreply.github.com>
2024-05-30 15:53:53 -0400

    update lualine z to show saved status

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,16 +1,20 @@
 return {
-	{
-		"nvim-lualine/lualine.nvim",
+  {
+    "nvim-lualine/lualine.nvim",
 
-		opts = function(_, opts)
-			opts.sections.lualine_z = {
-				{
-					"buffers",
-					icons_enabled = false,
-					mode = 1,
-					max_length = vim.o.columns * 1 / 8,
-				},
-			}
-		end,
-	},
+    opts = function(_, opts)
+      opts.sections.lualine_z = {
+        {
+          function()
+            for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+              if vim.bo[buf].modified then
+                return "󱙃"
+              end
+            end
+            return "󱣪"
+          end,
+        },
+      }
+    end,
+  },
 }
