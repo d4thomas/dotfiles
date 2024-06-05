fab9c5fe0cf9b300f8cfc511740c94833e57f879
iiixio <152221911+iiixio@users.noreply.github.com>
2024-06-04 22:08:53 -0400

    update keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,5 @@
 local map = LazyVim.safe_keymap_set
 
+map("n", "<F1>", "<cmd>Telescope keymaps<cr>", { desc = "Search Keymaps", remap = true })
 map("n", "<F5>", "<cmd>DiagnosticToggle<cr>", { desc = "Toggle Dianostics", remap = true })
+map("n", "<C-]>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
