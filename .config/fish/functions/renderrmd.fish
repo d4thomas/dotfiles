8d63ce1c8368cd2b8c72acbde6ef2f62ccdee44d
iiixio <152221911+iiixio@users.noreply.github.com>
2024-06-07 13:09:01 -0400

    remove fish for non-POSIX issues

--- .config/fish/functions/renderrmd.fish
+++
@@ -1,8 +0,0 @@
-# Quickly render Rmd files to html
-function renderrmd
-    if test -z "$argv[1]"
-        echo "Usage: renderrmd <file>"
-        return 1
-    end
-    Rscript -e "rmarkdown::render('$argv[1]')"
-end
