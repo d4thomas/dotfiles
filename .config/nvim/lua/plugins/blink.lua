e86b450e382078cc3fd2b268998697188681e606
iiixio <152221911+iiixio@users.noreply.github.com>
2024-12-15 19:23:42 -0500

    updates

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -5,6 +5,18 @@ return {
       opts.keymap = {
         preset = "super-tab",
       }
+      opts.sources = {
+        providers = {
+          copilot = {
+            name = "copilot",
+            module = "blink-cmp-copilot",
+          },
+        },
+        compat = {},
+        -- Not enabled: snippets, copilot
+        default = { "lsp", "path", "buffer" },
+        cmdline = {},
+      }
     end,
   },
 }
