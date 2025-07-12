02a7dd1d82e398985d14dacf433efa26430f9dfe
d4thomas <152221911+d4thomas@users.noreply.github.com>
2025-07-12 13:13:43 -0400

    add homebrew.zsh

---
+++ .config/zsh/homebrew.zsh
@@ -0,0 +1,16 @@
+# Configure Homebrew
+if command -v brew &> /dev/null; then
+    FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
+
+    export HOMEBREW_CASK_OPTS='--no-quarantine'
+    export HOMEBREW_NO_ENV_HINTS=1
+    export HOMEBREW_NO_EMOJI=1
+
+    alias brew-backup='brew bundle dump --file=~/.config/brew/Brewfile --force'
+    alias brew-cleanup='brew autoremove; brew cleanup --prune=all; brew cleanup -s'
+    alias brew-upgrade='brew upgrade; brew-cleanup'
+
+    if [ -f "$HOME/.config/brew/Brewfile" ]; then
+        alias brew-restore='brew bundle --file=~/.config/brew/Brewfile --force'
+    fi
+fi
